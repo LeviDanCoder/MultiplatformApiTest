4fb9917063780c60c5947738efb5cf9c6efa3afe
Levitchi Dan <dan.levitchi.extern@7-tv.de>
2022-08-16 12:47:30 +0300

    move to io

--- shared/src/androidMain/kotlin/com/example/ktormultiplatformsample1/ApplicationDispatcher.kt
+++ shared/src/androidMain/kotlin/com/example/ktormultiplatformsample1/ApplicationDispatcher.kt
@@ -3,4 +3,4 @@ package com.example.ktormultiplatformsample1
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
-internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.Default
+internal actual val ApplicationDispatcher: CoroutineDispatcher = Dispatchers.IO
